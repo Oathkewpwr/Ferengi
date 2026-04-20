df780d9a3b8222e57a2d990b44abfd3fcc696657
Juhong Liang <jliang25@students.desu.edu>
2026-04-20 06:37:06 -0400

    commit

--- src/main/java/edu/desu/cis/robot/control/MazeRobot.java
+++ src/main/java/edu/desu/cis/robot/control/MazeRobot.java
@@ -16,9 +16,69 @@ public class MazeRobot extends RobotController {
         super(robotName);
     }
 
+    public enum RobotState
+    {
+        CRUISE,
+        IDENTIFY_OBJECT,
+        AVOID_OBJECT,
+        MOVE_OBJECT,
+        COLLECT_SAMPLE,
+        RETURN,
+        STOP
+    }
 
-    public void run(){
-        // need to implement.
+    private RobotState state;
+
+
+    public void run() {
+        state = RobotState.CRUISE;
+        SensorSnapshot s;
+        while (state != RobotState.STOP) {
+            s = awaitNewData();// CORRECT — fresh snapshot every iteration
+            switch (state) {
+                case CRUISE:
+                    System.out.println("CRUISE");
+                    if (s.distance() < 20){
+                        mbot.stop();
+                        stete = RobotState.IDENTIFY_OBJECT;
+                    }
+                    break;
+                case IDENTIFY_OBJECT:
+                    System.out.println("IDENTIFY_OBJECT");
+                    String color = mbot.getColorObjectFromCamera();
+                    if (color.equals("blue")) {
+                        state == RobotState.AVOID_OBJECT;
+                    } else if (color.equals("green")) {
+                        state == RobotState.MOVE_OBJECT;
+                    }else if (color.equals("red")) {
+                        state = RobotState.COLLECT_SAMPLE;
+                    }
+                    break;
+                // ... other cases use s.distance(), s.lineStatus(), s.lineOffset()
+                case AVOID_OBJECT:
+                    //
+                    System.out.println("AVOID_OBJECT");
+                    break;
+                case MOVE_OBJECT:
+                    //
+                    System.out.println("MOVE_OBJECT");
+                    break;
+                case COLLECT_SAMPLE:
+                    //
+                    System.out.println("COLLECT_SAMPLE");
+                    break;
+                case RETURN:
+                    //
+                    System.out.println("RETURN");
+                    if (atInsertionPoint()){
+                        mbot.stop();
+                        System.out.println("VICTORY");
+                        state = RobotState.STOP;
+                    }
+                    break;
+
+            }
+        }
     }
 
     /**
